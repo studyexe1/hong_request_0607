793f01378474cd9de17ed015c05ea56f836033e2
studyexe1 <39167180+studyexe1@users.noreply.github.com>
2018-06-07 13:49:28 +0900

    dfd
    fdfdf

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//on/off스위치
+  $("#myonoffswitch").on('click', function(e) {
+    e.preventDefault();
+  })
+  // 눌럿을떄 바로 이동
+  $(".store_list").click(function() {
+      location.href="FoodTruck_Menu.html";
+  });
